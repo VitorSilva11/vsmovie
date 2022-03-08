2d95e2bcde9bfac65164ad3a7d548e52642808d4
VitorSilva11 <svitoryago@hotmail.com>
2022-03-08 15:26:21 -0300

    Project created

---
+++ backend/src/main/java/br/com/study/vsmovie/VsmovieApplication.java
@@ -0,0 +1,13 @@
+package br.com.study.vsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsmovieApplication.class, args);
+	}
+
+}
